368f5d18b183a51dca012a34ab320ea8a0e2b7e1
Deividas P <ngivingup@gmail.com>
2020-02-10 23:25:27 +0100

    pong game by Unity YouTube

---
+++ Assets/Scripts/Systems/PlayerInputSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+[AlwaysSynchronizeSystem]
+public class PlayerInputSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities.ForEach(( ref PaddleMovementData movementData, in PaddleInputData inputData) =>
+            {
+                movementData.Direction = 0;
+
+                movementData.Direction += Input.GetKey(inputData.upKey) ? 1 : 0;
+                movementData.Direction -= Input.GetKey(inputData.downKey) ? 1 : 0;
+            }).Run();
+
+        return default;
+    }
+}
